8305ab795fe673e568e88c26145ed801c1034da9
Robyn <scuadra02@gmail.com>
2024-09-02 15:11:25 -0400

    feat: make table dynamic

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
                   <button @click="add = true"
                     class="inline-flex ml-1.5 items-start justify-start px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded">
                     <p class="text-sm font-medium leading-none text-white">
-                      Add User
+                      Añadir
                     </p>
                   </button>
                 </div>
@@ -41,16 +41,121 @@
             <table class="w-full whitespace-nowrap">
               <thead>
                 <tr class="h-20 w-full text-sm leading-none text-gray-600">
-                  <th class="font-normal text-left pl-4">#</th>
-                  <th class="font-normal text-left pl-11">Name</th>
-                  <th class="font-normal text-left pl-10">Age</th>
-                  <th class="font-normal text-left">Created</th>
-                  <th class="font-normal text-left">Job Title</th>
-                  <th class="font-normal text-left">Status</th>
-                  <th class="font-normal text-left">Actions</th>
+                  <th class="font-normal text-center pl-4">RUT</th>
+                  <th class="font-normal text-center pl-11">Nombres</th>
+                  <th class="font-normal text-center">Apellidos</th>
+                  <th class="font-normal text-center pl-10">Dirección</th>
+                  <th class="font-normal text-center pl-6">Ciudad</th>
+                  <th class="font-normal text-center">Teléfono</th>
+                  <th class="font-normal text-center">Opciones</th>
                 </tr>
               </thead>
               <tbody class="w-full">
+                <tr v-for="item in items" class="h-20 text-sm leading-none text-gray-700 border-b border-t border-gray-200 bg-white hover:bg-gray-100">
+                  <td class="pl-4 text-center">
+                    {{ item.rut }}
+                  </td>
+                  <td class="pl-11 text-center">
+                    {{ item.name }}
+                  </td>
+                  <td class="text-center">
+                    {{ item.lastname }}
+                  </td>
+                  <td class="pl-10 text-center">
+                    {{ item.address }}
+                  </td>
+                  <td class="pl-6 text-center">
+                    {{ item.city }}
+                  </td>
+                  <td class="text-center">
+                    {{ item.phone }}
+                  </td>
+                  <td>
+                    <div class="flex items-center">
+                      <button
+                        class=" mr-3 hover:bg-gray-200 py-2.5 px-5 rounded text-sm leading-3 text-gray-500">
+                        Ver
+                      </button>
+                      <button
+                        class="bg-gray-100 mr-3 hover:bg-gray-200 py-2.5 px-5 rounded text-sm leading-3 text-gray-500">
+                        Editar
+                      </button>
+                      <button
+                        class="bg-gray-100 mr-5 hover:bg-gray-200 py-2.5 px-5 rounded text-sm leading-3 text-gray-500">
+                        Borrar
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+
+                <tr
+                  class="h-20 text-sm leading-none text-gray-700 border-b border-t border-gray-200 bg-white hover:bg-gray-100">
+                  <!-- Rut -->
+                  <td class="pl-4">1</td>
+                  <!-- Nombres -->
+                  <td class="pl-11">
+                    <div class="flex items-center">
+                      <img class="shadow-md rounded-full w-10 h-10 mr-3"
+                        src="https://cdn.tuk.dev/assets/templates/olympus/invoice.png" />
+                      Miracle Botos
+                    </div>
+                  </td>
+                  <!-- Apellidos -->
+                  <td>
+                    <p class="mr-16 pl-10">28</p>
+                  </td>
+                  <!-- Direccion -->
+                  <td>
+                    <p class="mr-16">06/02/2020</p>
+                  </td>
+                  <!-- Ciudad-->
+                  <td>
+                    <p class="mr-16">Product Designer</p>
+                  </td>
+                  <!-- Telefono -->
+                  <td>
+                    <div class="w-20 h-6 flex items-center mr-16 justify-center bg-blue-50 rounded-full">
+                      <p class="text-xs leading-3 text-blue-500">Approved</p>
+                    </div>
+                  </td>
+                  <!-- Opciones -->
+                  <td>
+                    <div class="flex items-center">
+                      <button
+                        class="bg-gray-100 mr-3 hover:bg-gray-200 py-2.5 px-5 rounded text-sm leading-3 text-gray-500">
+                        Edit
+                      </button>
+                      <button
+                        class="bg-gray-100 mr-5 hover:bg-gray-200 py-2.5 px-5 rounded text-sm leading-3 text-gray-500">
+                        Call
+                      </button>
+                      <div class="relative px-5 pt-2">
+                        <button class="focus:outline-none" @click="table1 = !table1">
+                          <svg class="dropbtn" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                            viewBox="0 0 20 20" fill="none">
+                            <path
+                              d="M4.16667 10.8332C4.62691 10.8332 5 10.4601 5 9.99984C5 9.5396 4.62691 9.1665 4.16667 9.1665C3.70643 9.1665 3.33334 9.5396 3.33334 9.99984C3.33334 10.4601 3.70643 10.8332 4.16667 10.8332Z"
+                              stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                              d="M10 10.8332C10.4602 10.8332 10.8333 10.4601 10.8333 9.99984C10.8333 9.5396 10.4602 9.1665 10 9.1665C9.53976 9.1665 9.16666 9.5396 9.16666 9.99984C9.16666 10.4601 9.53976 10.8332 10 10.8332Z"
+                              stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                              d="M15.8333 10.8332C16.2936 10.8332 16.6667 10.4601 16.6667 9.99984C16.6667 9.5396 16.2936 9.1665 15.8333 9.1665C15.3731 9.1665 15 9.5396 15 9.99984C15 10.4601 15.3731 10.8332 15.8333 10.8332Z"
+                              stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                          </svg>
+                        </button>
+                        <div v-if="table1" class="dropdown-content bg-white shadow w-24 absolute z-30 -ml-20">
+                          <div class="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
+                            <p>Edit</p>
+                          </div>
+                          <div class="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
+                            <p>Delete</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
                 <tr
                   class="h-20 text-sm leading-none text-gray-700 border-b border-t border-gray-200 bg-white hover:bg-gray-100">
                   <td class="pl-4">1</td>
@@ -782,7 +887,9 @@ export default {
   data() {
     return {
       add: false,
-      items: [],
+      items: [
+        { rut: '210006702', name: 'Robyn Maya', lastname: 'something something', address: '123 132 mansfosdmgssg', city: 'La Serena', phone: '1312312313', }
+      ],
     };
   },
   methods: {
